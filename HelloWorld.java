3e2553537b14fa213fbb68f722efd2dbb513a4db
Gosavi Prasanna Dattu <Prasannagosavi4@gmail.com>
2025-11-27 13:48:43 +0530

    Rename class and update print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
-public class Main {
+public class HelloWorldOP {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Prasanna");
+        System.out.println("Hello, World");
     }
 }
